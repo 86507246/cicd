11255d68336c1e79698974eb9e6c5f51a1239688
Anton Ermolenko <aermolenko@atlassian.com>
2018-08-22 12:36:10 +1000

    BBSDEV-18005 NFS support in scripts
    Modified install script so it accepts a parameter to specify what to
    configure - NFS server or Bitbucket Server node. In case of unsupported
    installation option it exists with a log message.

--- bitbucket/scripts/install.sh
+++ bitbucket/scripts/install.sh
@@ -3,9 +3,27 @@
 source ./log.sh
 
 function prepare_datadisks {
-  log prepare_datadisks "Preparing data disks, striping, adding to fstab"
+  log "Preparing data disks, striping, adding to fstab"
   ./vm-disk-utils-0.1.sh -b "/datadisks" -o "noatime,nodiratime,nodev,noexec,nosuid,nofail,barrier=0"
-  log prepare_datadisks "Done preparing and configuring data disks"
+  log "Done preparing and configuring data disks"
 }
 
-prepare_datadisks
+function install_nfs {
+    log "Configuring NFS server..."
+    prepare_datadisks
+}
+
+function install_bbs {
+    log "Configuration Bitbucket Server node..."
+    prepare_datadisks
+}
+
+function install_unsupported {
+    error "Unsupported installation option, abort"
+}
+
+case "$1" in
+    nfs) install_nfs;;
+    bbs) install_bbs;;
+    *) install_unsupported;;
+esac
