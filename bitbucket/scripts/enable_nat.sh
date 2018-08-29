074d3595b5dc5fd72a37461b7ac642204817c315
Anton Ermolenko <aermolenko@atlassian.com>
2018-08-29 17:48:27 +1000

    BBSDEV-18223 added Load Balancer and scripts
    Turns out that Application Gateway can't be configured to use custom
    load balancer because it's already using one behind the scenes. There is
    no way to replace this default load balancer with our custom one as
    well. There for we need additional hop for our HTTP traffic. Load
    Balancer will send HTTP traffic to a linux VM which will do port
    forwarding to the Application Gateway IP address.

---
+++ bitbucket/scripts/enable_nat.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source ./log.sh
+
+SOURCE_PORT="80"
+TARGET_PORT="80"
+
+function enable_port_forwarding {
+    local target = "${1}"
+
+    log "Enabling port forwarding for HTTP traffic [target ip=${target}]"
+
+    sysctl net.ipv4.ip_forward=1
+
+
+    iptables -t nat -A PREROUTING -p tcp -i eth0 --dport "${TARGET_PORT}" -j DNAT --to-destination "${target}"
+    iptables -A FORWARD -i eth0 -p tcp -d "${target}"--dport "${TARGET_PORT}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+    iptables -t nat -A POSTROUTING -j MASQUERADE
+
+    log "Done enabling port forwarding for HTTP traffic [target ip=${target}]!"
+}
