735d868c5e33322167fabc6a0e4ec8598a80f50d
Anton Ermolenko <aermolenko@atlassian.com>
2018-09-06 14:05:02 +1000

    BBSDEV-18007 NAT implementation in progress
    Added a route table for BBS network
    Added second NIC for jumpbox - to be included into BBS subnet
    Changed load balancer to have private IP instead of public one
    Changed jumpbox to be the gateway for the vnet

--- bitbucket/scripts/enable_nat.sh
+++ bitbucket/scripts/enable_nat.sh
@@ -2,26 +2,29 @@
 
 source ./log.sh
 
-SOURCE_PORT="80"
-TARGET_PORT="80"
-
 function enable_port_forwarding {
     local target="${1}"
+    local port="${2}"
 
-    log "Enabling port forwarding for HTTP traffic [target ip=${target}]"
+    log "Enabling port forwarding for [port=${port}] traffic [target ip=${target}]"
 
     sysctl net.ipv4.ip_forward=1
 
-    iptables -t nat -A PREROUTING -p tcp -i eth0 --dport "${TARGET_PORT}" -j DNAT --to-destination "${target}"
-    iptables -A FORWARD -i eth0 -p tcp -d "${target}" --dport "${TARGET_PORT}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+    iptables -t nat -A PREROUTING -p tcp -i eth0 --dport "${port}" -j DNAT --to-destination "${target}"
+    iptables -A FORWARD -i eth0 -p tcp -d "${target}" --dport "${port}" -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
-    iptables -t nat -A POSTROUTING -j MASQUERADE
+    log "Done enabling port forwarding for [port=${port}] traffic [target ip=${target}]!"
+}
 
-    log "Done enabling port forwarding for HTTP traffic [target ip=${target}]!"
+function enable_nat {
+    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+    iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 }
 
 if [ "x$1" == "x" ]; then
-    log "Usage: enable_nat _target_ip_"
+    log "Usage: enable_nat _app_gw_ip_ _git_lb_ip_"
 else
-    enable_port_forwarding "$1"
+    enable_port_forwarding "$1" 80
+    enable_port_forwarding "$1" 443
+    enable_port_forwarding "$2" 7999
 fi
